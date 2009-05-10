c3f29c79ead1e9e084a83fc05070a6a5b2cae86f
Daniel Higginbotham <daniel@daniel-higginbothams-macbook.local>
2009-05-10 00:55:44 -0400

    renaming a constant

--- aikidoka.rb
+++ aikidoka.rb
@@ -11,14 +11,14 @@ Object.send(:remove_const, :Mash)
 puts Namespace::Mash
 
 module Aikidoka
-  # Keys should be new names,
-  # Values should be names of classes/modules to rename
+  # Keys should be old names of classes/modules to rename,
+  # Values should be new names
   def self.rename(pairs = {})
-    create_modules(pairs.keys)
+    create_modules(*pairs.values)
     #protect_existing_constants
     yield
     copy_constants(pairs)
-    remove_constants(pairs.values)
+    remove_constants(*pairs.keys)
   end
   
   def self.create_modules(*module_names)
@@ -32,7 +32,7 @@ module Aikidoka
   end
   
   def self.copy_constants(pairs)
-    pairs.each do |new_name, old_name|
+    pairs.each do |old_name, new_name|
       eval("#{new_name}= #{old_name}")
     end
   end
--- spec/aikidoka_spec.rb
+++ spec/aikidoka_spec.rb
@@ -8,10 +8,23 @@ describe Aikidoka do
     Weapons.const_defined?(:Bokken).should be_false
   end
   
-  it "should remove constnts" do
+  it "should remove constants" do
     module Weapons; end
     Object.const_defined?(:Weapons).should be_true
     Aikidoka.remove_constants(:Weapons)
     Object.const_defined?(:Weapons).should be_false
+  end
+  
+  it "should rename a top-level constant" do
+    Aikidoka.rename("Weapons" => "JapaneseArts::Weapons") do 
+      module Weapons
+        def strike
+        end
+      end
+    end
+    Object.const_defined?(:Weapons).should be_false
+    Object.const_defined?(:JapaneseArts).should be_true
+    JapaneseArts.const_defined?(:Weapons).should be_true
+    JapaneseArts::Weapons.method_defined?(:strike).should be_true
   end
 end
