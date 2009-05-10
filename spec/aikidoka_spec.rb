fc0d4e8f69cb91d3086b825b1a37c5d685e56c12
Daniel Higginbotham <daniel@daniel-higginbothams-macbook.local>
2009-05-10 01:37:01 -0400

    properly cleaning up after each spec

--- spec/aikidoka_spec.rb
+++ spec/aikidoka_spec.rb
@@ -6,27 +6,49 @@ describe Aikidoka do
     Aikidoka.create_modules("Weapons::Bokken")
     Object.const_defined?(:Weapons).should be_true
     Weapons.const_defined?(:Bokken).should be_false
+    
+    remove_constant_hierarchy("Weapons")
   end
   
   it "should create modules/classes for all but most-nested constant name when constants with those names exist" do
-    module JapaneseArts; class Weapons; class Bokken; end end end
-    Aikidoka.create_modules("JapaneseArts::Weapons::Bokken")
+    module Aikido; class Weapons; class Bokken; end end end
+    Aikidoka.create_modules("Aikido::Weapons::Bokken")
+    
+    remove_constant_hierarchy("Aikido::Weapons")
   end
   
   it "should remove constants" do
-
+    module Weapons; end
+    Object.const_defined?(:Weapons).should be_true
+    Aikidoka.remove_constants(:Weapons)
+    Object.const_defined?(:Weapons).should be_false
   end
   
   it "should rename a top-level constant" do
-    Aikidoka.rename("Weapons" => "JapaneseArts::Weapons") do 
+    Aikidoka.rename("Weapons" => "Aikido::Weapons") do 
       module Weapons
         def strike
         end
       end
     end
     Object.const_defined?(:Weapons).should be_false
-    Object.const_defined?(:JapaneseArts).should be_true
-    JapaneseArts.const_defined?(:Weapons).should be_true
-    JapaneseArts::Weapons.method_defined?(:strike).should be_true
+    Object.const_defined?(:Aikido).should be_true
+    Aikido.const_defined?(:Weapons).should be_true
+    Aikido::Weapons.method_defined?(:strike).should be_true
+    
+    remove_constant_hierarchy("Aikido::Weapons")
+  end
+end
+
+# each name is i.e. Aikido::Weapons::Bokken
+def remove_constant_hierarchy(*names)
+  names.each do |name|
+    pieces = name.split("::")
+    pieces.size.downto(1).each do |size|
+      to_remove = pieces.pop
+      const_name = pieces.join("::")
+      parent = Object.class_eval(const_name) || Object
+      parent.send(:remove_const, to_remove)
+    end
   end
 end
