51ffab0f38d9ac5d8a3feab1ff4567727e2fba5b
Daniel Higginbotham <daniel@daniel-higginbothams-macbook.local>
2009-05-10 00:50:01 -0400

    removing constants

--- aikidoka.rb
+++ aikidoka.rb
@@ -15,15 +15,31 @@ module Aikidoka
   # Values should be names of classes/modules to rename
   def self.rename(pairs = {})
     create_modules(pairs.keys)
-    
+    #protect_existing_constants
+    yield
+    copy_constants(pairs)
+    remove_constants(pairs.values)
   end
   
   def self.create_modules(*module_names)
     module_names.uniq.each do |name|
       pieces = name.split("::")
+      pieces.pop
       module_definition = pieces.reverse.inject(""){|definition, piece| "module #{piece}; #{definition} end"}
       puts module_definition
       Object.class_eval(module_definition)
+    end
+  end
+  
+  def self.copy_constants(pairs)
+    pairs.each do |new_name, old_name|
+      eval("#{new_name}= #{old_name}")
+    end
+  end
+  
+  def self.remove_constants(*modules_names)
+    modules_names.each do |name|
+      Object.send(:remove_const, name)
     end
   end
 end
--- spec/aikidoka_spec.rb
+++ spec/aikidoka_spec.rb
@@ -1,14 +1,17 @@
 require 'aikidoka'
 
 describe Aikidoka do
-  it "should create un-nested module" do
+  it "should create modules for all but most-nested constant name" do
     Object.const_defined?(:Weapons).should be_false
-    Aikidoka.create_modules("Weapons")
+    Aikidoka.create_modules("Weapons::Bokken")
     Object.const_defined?(:Weapons).should be_true
+    Weapons.const_defined?(:Bokken).should be_false
   end
   
-  it "should create nested module" do
-    Aikidoka.create_modules("Weapons::Bokken")
-    Weapons.const_defined?("Bokken").should be_true
+  it "should remove constnts" do
+    module Weapons; end
+    Object.const_defined?(:Weapons).should be_true
+    Aikidoka.remove_constants(:Weapons)
+    Object.const_defined?(:Weapons).should be_false
   end
 end
